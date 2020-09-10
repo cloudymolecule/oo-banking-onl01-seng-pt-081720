e90c3347fd12907116952ed592781bafc0a0a6da
Dario Carlino <baires82@gmail.com>
2020-09-10 01:19:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
         self.status = "complete"
     # else
     #   self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      #return "Transaction rejected. Please check your account balance."
     end
 
 

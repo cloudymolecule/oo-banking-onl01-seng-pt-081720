fd81174ac7b36fdb94fff804c532167ab8b897d0
Dario Carlino <baires82@gmail.com>
2020-09-10 01:07:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
 
   def execute_transaction
     #binding.pry
-    if sender.valid?
+    if @sender.valid?
         @sender.balance = (@sender.balance - @amount)
         @receiver.balance += @amount
         @amount = 0

69288db08d9212b3391caee17f3993d3c7abea8f
Dario Carlino <baires82@gmail.com>
2020-09-10 01:38:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,8 +34,9 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @receiver.balance = (@receiver.balance - @reversio)
-      @sender.balance + @reversio
+      @receiver.balance = (@receiver.balance - @amount)
+      @sender.balance + @amount
+      
     end
   end
 

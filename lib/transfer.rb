9b89778ab9f50751b72b53b7ea0614acfc9e359b
Dario Carlino <baires82@gmail.com>
2020-09-10 01:15:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
 
   def execute_transaction
 
-    if self.sender.balance >= @amount && self.sender.status == "oen"
+    if self.sender.balance >= @amount && self.sender.status == "open"
         @sender.balance = (@sender.balance - @amount)
         @receiver.balance += @amount
         @amount = 0
